85e171787f0e4de411ae920b445e9efa6efaf379
t1lear00 <t1lear00@students.oamk.fi>
2022-11-02 12:11:54 +0200

    gy-61 sensori luku arduinolla

--- arduinostuff/tietoprojektitest.ino
+++ arduinostuff/tietoprojektitest.ino
@@ -1,18 +1,53 @@
 /*
-Tulosta numeroita ikiloopissa sarjaporttiin
+Tulosta Kiihtyvyys anturin raaka dataa serial plotterille
 */
+const int vccpin = A0;
+const int xpin = A1;
+const int ypin = A2;
+const int zpin = A3;
+const int GNDpin = A4;
+//alustetaan muutujat
+int x = 0;
+int y = 0;
+int z = 0;
+int ka = 0;
+
+
 void setup()
 {
   Serial.begin(9600); // sarjaportin nopeus
+  //sensori pinnit
+  pinMode(vccpin, OUTPUT);
+  pinMode(GNDpin, OUTPUT);
+  //jännite 5v ja gnd 0v
+   digitalWrite(vccpin, HIGH);
+   delayMicroseconds(2); 
+   digitalWrite(GNDpin, LOW); 
+   delayMicroseconds(2);
 }
 
 int number = 0;
 
 void loop()
 {
-  Serial.print("Numero on ");
-  Serial.println(number);    // tulosta numero
+  for(ka = 0; ka < 20; ka++) //20 mittauksen keskiarvo tarkkuuden parantamisesksi
+  {
+    x = x + analogRead(xpin);  // sensori arvot 0-1023
+    y = y + analogRead(ypin);
+    z = z + analogRead(zpin);
+  }
 
-  delay(500); // 0.5 s viive
-  number++; 
+  x = x / 20;
+  y = y / 20;
+  z = z / 20;
+  Serial.println("X, Y, Z  ");
+  Serial.print(x);
+  Serial.print(" "); 
+  Serial.print(y);
+  Serial.print(" ");    
+  Serial.println(z);
+     
+
+  delay(1000); 
+  
 }
